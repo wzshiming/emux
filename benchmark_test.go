2179f85ffafba806d05b861728fd01fc8817f9cd
Shiming Zhang <wzshiming@foxmail.com>
2021-11-10 23:32:21 +0800

    Update benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,336 +7,167 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"strings"
+	"sync"
 	"testing"
 )
 
-func BenchmarkSerialEmux(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(200)
-	}))
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		b.Fatal(err)
+func BenchmarkAll(b *testing.B) {
+	for _, serverFunc := range serverCases {
+		for _, aliveFunc := range aliveCases {
+			for _, wayFunc := range wayCases {
+				for _, connFunc := range connCases {
+					func() {
+						serverName, server := serverFunc(b)
+						aliveName, alive := aliveFunc(b)
+						wayName, way := wayFunc(b)
+						connName, dialer, listener := connFunc(b)
+						server.Listener = listener
+						server.Start()
+						defer server.Close()
+						cli := server.Client()
+						cli.Transport = &http.Transport{
+							DisableKeepAlives: alive,
+							DialContext:       dialer.DialContext,
+						}
+						b.Run(strings.Join([]string{serverName, aliveName, wayName, connName}, "-"), func(b *testing.B) {
+							way(b, cli, server)
+						})
+					}()
+				}
+			}
+		}
 	}
-	svc.Listener = NewListener(context.Background(), l)
-	svc.Start()
-	defer svc.Close()
+}
 
-	dialer := NewDialer(&net.Dialer{})
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: true,
-		DialContext:       dialer.DialContext,
-	}
-	for n := 0; n != b.N; n++ {
-		resp, err := cli.Get(svc.URL)
+var wayCases = []func(b *testing.B) (string, func(b *testing.B, cli *http.Client, server *httptest.Server)){
+	func(b *testing.B) (string, func(b *testing.B, cli *http.Client, server *httptest.Server)) {
+		return "serial", func(b *testing.B, cli *http.Client, server *httptest.Server) {
+			for n := 0; n != b.N; n++ {
+				resp, err := cli.Get(server.URL)
+				if err != nil {
+					b.Fatal(err)
+				}
+				if resp.StatusCode != 200 {
+					b.Fatal("expected 200, got", resp.StatusCode)
+				}
+				io.Copy(io.Discard, resp.Body)
+				resp.Body.Close()
+			}
+		}
+	},
+	func(b *testing.B) (string, func(b *testing.B, cli *http.Client, server *httptest.Server)) {
+		return "parallel", func(b *testing.B, cli *http.Client, server *httptest.Server) {
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					resp, err := cli.Get(server.URL)
+					if err != nil {
+						b.Fatal(err)
+					}
+					if resp.StatusCode != 200 {
+						b.Fatal("expected 200, got", resp.StatusCode)
+					}
+					io.Copy(io.Discard, resp.Body)
+					resp.Body.Close()
+				}
+			})
+		}
+	},
+}
+
+var aliveCases = []func(b *testing.B) (string, bool){
+	func(b *testing.B) (string, bool) {
+		return "disableKeepAlives", true
+	},
+	func(b *testing.B) (string, bool) {
+		return "keepAlives", false
+	},
+}
+
+var serverCases = []func(b *testing.B) (string, *httptest.Server){
+	func(b *testing.B) (string, *httptest.Server) {
+		return "nobody", httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			rw.WriteHeader(200)
+		}))
+	},
+	func(b *testing.B) (string, *httptest.Server) {
+		return "stream", httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			io.Copy(rw, io.LimitReader(rand.Reader, 1024*1024))
+		}))
+	},
+	func(b *testing.B) (string, *httptest.Server) {
+		return "packet", httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			io.Copy(rw, io.LimitReader(rand.Reader, 10))
+		}))
+	},
+}
+
+var connCases = []func(b *testing.B) (string, Dialer, net.Listener){
+	func(b *testing.B) (string, Dialer, net.Listener) {
+		l := &net.ListenConfig{}
+		t, err := l.Listen(context.Background(), "tcp", ":0")
 		if err != nil {
 			b.Fatal(err)
 		}
-		if resp.StatusCode != 200 {
-			b.Fatal("expected 200, got", resp.StatusCode)
-		}
-		resp.Body.Close()
-	}
-}
-
-func BenchmarkSerialOrigin(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(200)
-	}))
-	svc.Start()
-	defer svc.Close()
-
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: true,
-	}
-	for n := 0; n != b.N; n++ {
-		resp, err := cli.Get(svc.URL)
+		return "tcpHttp", &net.Dialer{}, t
+	},
+	func(b *testing.B) (string, Dialer, net.Listener) {
+		l := &net.ListenConfig{}
+		t, err := l.Listen(context.Background(), "tcp", ":0")
 		if err != nil {
 			b.Fatal(err)
 		}
-		if resp.StatusCode != 200 {
-			b.Fatal("expected 200, got", resp.StatusCode)
-		}
-		resp.Body.Close()
+		return "tcpHttpEmux", NewDialer(&net.Dialer{}), NewListener(context.Background(), t)
+	},
+	func(b *testing.B) (string, Dialer, net.Listener) {
+		l := newTestPipeServer()
+		return "pipeHttp", l, l
+	},
+	func(b *testing.B) (string, Dialer, net.Listener) {
+		l := newTestPipeServer()
+		return "pipeHttpEmux", NewDialer(l), NewListener(context.Background(), l)
+	},
+}
+
+type testPipeServer struct {
+	accept  chan net.Conn
+	addr    net.Addr
+	once    sync.Once
+	isClose bool
+}
+
+func newTestPipeServer() *testPipeServer {
+	return &testPipeServer{
+		accept: make(chan net.Conn, 1),
+		addr:   &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 80},
 	}
 }
 
-func BenchmarkSerialOriginKeepAlives(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(200)
-	}))
-	svc.Start()
-	defer svc.Close()
-
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: false,
+func (t *testPipeServer) Accept() (net.Conn, error) {
+	conn, ok := <-t.accept
+	if !ok {
+		return nil, net.ErrClosed
 	}
-	for n := 0; n != b.N; n++ {
-		resp, err := cli.Get(svc.URL)
-		if err != nil {
-			b.Fatal(err)
-		}
-		if resp.StatusCode != 200 {
-			b.Fatal("expected 200, got", resp.StatusCode)
-		}
-		resp.Body.Close()
-	}
+	return conn, nil
 }
 
-func BenchmarkParallelEmux(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(200)
-	}))
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		b.Fatal(err)
-	}
-	listener := NewListener(context.Background(), l)
-	svc.Listener = listener
-	svc.Start()
-	defer svc.Close()
-
-	dialer := NewDialer(&net.Dialer{})
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: true,
-		DialContext:       dialer.DialContext,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			resp.Body.Close()
-		}
+func (t *testPipeServer) Close() error {
+	t.once.Do(func() {
+		t.isClose = true
+		close(t.accept)
 	})
+	return nil
 }
 
-func BenchmarkParallelOrigin(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(200)
-	}))
-	svc.Start()
-	defer svc.Close()
-
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: true,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			resp.Body.Close()
-		}
-	})
+func (t *testPipeServer) Addr() net.Addr {
+	return t.addr
 }
 
-func BenchmarkParallelOriginKeepAlives(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(200)
-	}))
-	svc.Start()
-	defer svc.Close()
-
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: false,
+func (t *testPipeServer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	if t.isClose {
+		return nil, net.ErrClosed
 	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			resp.Body.Close()
-		}
-	})
-}
-
-func BenchmarkParallelEmuxStream(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		io.Copy(rw, io.LimitReader(rand.Reader, 1024*1024))
-	}))
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		b.Fatal(err)
-	}
-	listener := NewListener(context.Background(), l)
-	svc.Listener = listener
-	svc.Start()
-	defer svc.Close()
-
-	dialer := NewDialer(&net.Dialer{})
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: true,
-		DialContext:       dialer.DialContext,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			io.Copy(io.Discard, resp.Body)
-			resp.Body.Close()
-		}
-	})
-}
-
-func BenchmarkParallelOriginStream(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		io.Copy(rw, io.LimitReader(rand.Reader, 1024*1024))
-	}))
-	svc.Start()
-	defer svc.Close()
-
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: true,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			io.Copy(io.Discard, resp.Body)
-			resp.Body.Close()
-		}
-	})
-}
-
-func BenchmarkParallelOriginStreamKeepAlives(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		io.Copy(rw, io.LimitReader(rand.Reader, 1024*1024))
-	}))
-	svc.Start()
-	defer svc.Close()
-
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: false,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			io.Copy(io.Discard, resp.Body)
-			resp.Body.Close()
-		}
-	})
-}
-
-func BenchmarkParallelEmuxMicroPacketStream(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		io.CopyBuffer(rw, io.LimitReader(rand.Reader, 1024*1024), make([]byte, 10))
-	}))
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		b.Fatal(err)
-	}
-	listener := NewListener(context.Background(), l)
-	svc.Listener = listener
-	svc.Start()
-	defer svc.Close()
-
-	dialer := NewDialer(&net.Dialer{})
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: true,
-		DialContext:       dialer.DialContext,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			io.Copy(io.Discard, resp.Body)
-			resp.Body.Close()
-		}
-	})
-}
-
-func BenchmarkParallelOriginMicroPacketStream(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		io.CopyBuffer(rw, io.LimitReader(rand.Reader, 1024*1024), make([]byte, 10))
-	}))
-	svc.Start()
-	defer svc.Close()
-
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: true,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			io.Copy(io.Discard, resp.Body)
-			resp.Body.Close()
-		}
-	})
-}
-
-func BenchmarkParallelOriginMicroPacketStreamKeepAlives(b *testing.B) {
-	svc := httptest.NewUnstartedServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		io.CopyBuffer(rw, io.LimitReader(rand.Reader, 1024*1024), make([]byte, 10))
-	}))
-	svc.Start()
-	defer svc.Close()
-
-	cli := svc.Client()
-	cli.Transport = &http.Transport{
-		DisableKeepAlives: false,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			resp, err := cli.Get(svc.URL)
-			if err != nil {
-				b.Fatal(err)
-			}
-			if resp.StatusCode != 200 {
-				b.Fatal("expected 200, got", resp.StatusCode)
-			}
-			io.Copy(io.Discard, resp.Body)
-			resp.Body.Close()
-		}
-	})
+	c1, c2 := net.Pipe()
+	t.accept <- c1
+	return c2, nil
 }
